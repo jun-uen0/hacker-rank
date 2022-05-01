c48f003b457d7921823a3b35879dfdd43d5078d9
jun-uen0 <hello@junueno.dev>
2022-05-01 18:30:35 +0900

    add 6,7 py

---
+++ challenges/006_python-arithmetic-operators.py
@@ -0,0 +1,23 @@
+# misread the problem
+# import numpy as np
+# a = 8
+# b = -2
+# c = 15
+# divisor = []
+# for num in range(1, c + 1):
+#   if(15 % num == 0):
+#     divisor.append(num)
+
+# print(divisor)
+
+# diff = np.diff(divisor)
+# print(diff)
+# ------------------------
+
+if __name__ == '__main__':
+  a = int(input())
+  b = int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
