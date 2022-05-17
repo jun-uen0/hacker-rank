e82216615b20973934b8aa5e0ce25c9726d26c3b
jun-uen0 <hello@junueno.dev>
2022-05-18 02:16:41 +0900

    added js submission

---
+++ javascript-challenges/find-second-largest-number-in-an-array.js
@@ -0,0 +1,35 @@
+'use strict'
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+let inputString = ''
+let currentLine = 0
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin
+})
+process.stdin.on('end', _ => {
+  inputString = inputString.trim().split('\n').map(string => {
+    return string.trim()
+  })
+  
+  main()
+})
+
+const readLine = () => {
+  return inputString[currentLine++]
+}
+
+const getSecondLargest = (nums) => {
+  const sArray = nums.sort((a, b) => {
+    return a - b
+  })
+  const fArray = sArray.filter((ex, idx, self) => {
+    return idx == self.indexOf(ex)
+  })
+  return fArray[fArray.length - 2]
+}
+
+const main = () => {
+  const firstNum = +(readLine())
+  const nums = readLine().split(' ').map(Number)  
+  console.log(getSecondLargest(nums))
+}
